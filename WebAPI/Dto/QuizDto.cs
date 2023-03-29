57f1e3a0023a49eb49b0cc29a86acf9fbc3c39bd
Piotrek5994 <piotrek5994@gmail.com>
2023-03-29 14:54:34 +0200

    class QuizDto

---
+++ WebAPI/Dto/QuizDto.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Models;
+
+namespace WebAPI.Dto
+{
+    public class QuizDto
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public List<QuizItemDto> Items { get; set; }
+
+
+        public static QuizDto of(Quiz quiz)
+        {
+            List<QuizItemDto> items = new List<QuizItemDto>();
+            foreach (QuizItem item in quiz.Items)
+            {
+                items.Add(QuizItemDto.of(item));
+            }
+            return new QuizDto
+            {
+                Id = quiz.Id,
+                Title = quiz.Title,
+                Items = items
+            };
+        }
+    }
+}
